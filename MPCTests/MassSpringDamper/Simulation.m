8e0c60e5f82a2141bca9d06307ae9c97abf7f41c
Thomas Frekhaug <95frekhaug@gmail.com>
2019-09-18 15:57:04 +0200

    18.9

--- MPCTests/MassSpringDamper/Simulation.m
+++ MPCTests/MassSpringDamper/Simulation.m
@@ -2,48 +2,46 @@ function [x,u,xopt,uopt,t]= Simulation(A,B,Q,R,x0,xRef,Xlb,Xub,Ulb,Uub,h,tspan,N
 %% Multiple shooting, Linear model
 %% Initialization
 %-----------Define the states and inputs---------
-    %X = State variables, U = control inputs, xnext = Euler progression
-    %Obj = Cost function %g2 = Equality constraints (System dynamics);
-    %g1 = Inequality constraints (inputs/state limits) 
-    X  = casadi.SX.sym('X',nx,N+1);
-    U  = casadi.SX.sym('U',nu,N);
-
-    P = casadi.SX.sym('P',nx*2);
-   % xnext = casadi.SX(nx,N);
-    stageCost = casadi.SX(N,1);
-    obj = 0;
-    g1 = 0;
-    g2 = casadi.SX(nx*N+nx,1);
+    X  = casadi.SX.sym('X',nx,N+1); %State/decision variables
+    U  = casadi.SX.sym('U',nu,N);   %Input/decision variables
+    P = casadi.SX.sym('P',nx*2);    %Inital and reference values
+    stageCost = casadi.SX(N,1);     %Vector of Stagecost/objective function
+    obj = 0;                        %Objective function -> sum of stageCost
+    g2 = casadi.SX(nx*N+nx,1);      %Vector of equality constraints
     
 %----------Setup of NLP--------------    
-    %Constraints: 
-
+%Calculation of stagecost, Objective function, and constraints
     g2(1:nx) = X(:,1) - P(1:nx);
     for i = 1:N
-
-       xnext = X(:,i) + T/N*(A*X(:,i) + B*U(:,i));
+       g2(i*nx+1:(i+1)*nx) = X(:,i+1) - (X(:,i) + T/N*(A*X(:,i) + B*U(:,i)));
+       
        %stageCost(i) = 0.5*(X(:,i)-P(nx+1:end))'*Q*(X(:,i)-P(nx+1:end)) + 0.5*U(:,i)'*R*(U(:,i));
        stageCost(i) = 0.5*(X(:,i+1))'*Q*(X(:,i+1)) + 0.5*U(:,i)'*R*(U(:,i));
+       
        obj = obj + stageCost(i);
-       g2(i*nx+1:(i+1)*nx) = X(:,i+1) - xnext;
+       
     end
-
-    %Optimization variables
+% Define Optimization variables and the nlpProblem
     OPTVariables = [reshape(X,nx*(N+1),1); reshape(U,nu*N,1)];
     nlpProb = struct('f',obj,'x',OPTVariables, 'g',g2,'p',P);
 %--------Arguments & Options------------
+%Options
     opts = struct;
     opts.ipopt.max_iter = 100;
-    opts.ipopt.print_level = 5;
-    opts.print_time = 1;
+    opts.ipopt.print_level = 0;
+    opts.print_time = 0;
     opts.ipopt.acceptable_tol = 1e-8;
     opts.ipopt.acceptable_obj_change_tol = 1e-6;
     solver = casadi.nlpsol('solver', 'ipopt', nlpProb,opts);   
-    % lbg&ubg = lower and upper bounds on the equality constraint
-    % lbx&ubx = lower and upper bounds on the decision states;
+%Arguments
     args = struct;
-    args.lbg(1:nx*(N+1),1) = 0;
+% lbg&ubg = lower and upper bounds on the equality constraint
+    args.lbg(1:nx*(N+1),1) = 0; 
     args.ubg(1:nx*(N+1),1) = 0;
+
+%lbx&ubx = lower and upper bounds on the decision states;
+%Checking for any upper or lower bounds, and adding them to args if
+%existing.
     for i = 1:nx
         if ~isempty(Xlb) % Lower bounds on X
             args.lbx(i:nx:nx*(N+1),1) = Xlb(i); 
@@ -71,68 +69,86 @@ function [x,u,xopt,uopt,t]= Simulation(A,B,Q,R,x0,xRef,Xlb,Xub,Ulb,Uub,h,tspan,N
     
 %% Simulation loop
 %------Output Variables-----
-    t = [0;tspan];
-    x = zeros(tspan/h,size(x0,1));
-    u = zeros(tspan/h,1);
-    uopttmp = zeros(tspan/h*nu,N);
-    xopttmp = zeros(tspan/h*nx,N+1);
+    x = zeros(tspan/h,size(x0,1));      %Matrix of resulting states
+    u = zeros(tspan/h,1);               %Matrix of resulting inputs
+    xopttmp = zeros(tspan/h*nx,N+1);    %Matrix of all optimal trajetories at each optitime t
+    uopttmp = zeros(tspan/h*nu,N);      %Matrix of all optimal inputs at each optitime t
+    
+    
+    %X0 = repmat(x0,1,N+1); 
+    X0 = zeros(2,N+1);                  %Initial search area for X variables
+    U0 = zeros(N,nu);                   %Initial search area for U variables
+    xx = zeros(nx,N);                   %Vector of current optimal trajectory
+    uu = zeros(nu,N);                   %Vector of current optimal input
+
+  
     x(1,:) = x0;
-	mpciter = 1; 
-    X0 = repmat(x0,1,N+1); U0 = zeros(N,nu); 
-    X0 = zeros(2,N+1);
-    uu = zeros(nu,N); xx = zeros(nx,N);
-    inputTime = 0;
+	mpciter = 0;                        %Counter for tracking simulation iterations 
+    inputTime = 0;                      %Timer for when new optimalization is due.
+%Start of simulation   
     while(norm((x0-xRef),2)> 1e-3 && mpciter<tspan/h)
-        t = h*(mpciter-1);
+        mpciter = mpciter + 1;
+        t = h*(mpciter-1);              %current simulation time
+        
         if t>=inputTime
-            args.p = [x0;xRef]; % Set the current values of reference and x0
+            % Set the current values of references and x0 in args.p
+            % Set the search space for next opti in args.x0
+            args.p = [x0;xRef]; 
             args.x0 = [reshape(X0',nx*(N+1),1); reshape(U0',nu*N,1)];
+            
+            %Solves the NLP
             sol = solver('x0',args.x0, 'lbx',args.lbx,'ubx',args.ubx,'lbg',...
             args.lbg,'ubg',args.ubg,'p',args.p);
-            disp("Optimal Input")
-            sol
+            
             %Extract inputs and path. Disp in proper form
             uu  = reshape(full(sol.x(nx*(N+1)+1:end))',nu,N); 
             xx  = reshape(full(sol.x(1:nx*(N+1)) ),nx,N+1);     
-            
+            %Store optimal solution in matrix
             xopttmp((mpciter-1)*nx + 1:(mpciter-1)*nx +nx ,1:N+1) = xx;
             uopttmp((mpciter-1)*nu + 1:(mpciter-1)*nu +nu ,1:N) = uu;
+            %Next optimalization time
             inputTime = inputTime + T/N;
-            [LSym,xSym,uSym,chiSym,cost] = Lagrangian(T,N,nx,nu,Q,R,A,B,xx,uu,sol,args,nlpProb);            
+            %Symbolic calculation of lagrangian, and resulting sensitivites
+            %analysis.
+            %[LSym,xSym,uSym,chiSym,cost] = Lagrangian(T,N,nx,nu,Q,R,A,B,xx,uu,sol,args,nlpProb);            
         else 
+            %No optimalization, store as nan in order to ease plotting
             xopttmp((mpciter-1)*nx + 1:(mpciter-1)*nx +nx ,1:N+1) = nan;
             uopttmp((mpciter-1)*nu + 1:(mpciter-1)*nu +nu ,1:N) = nan;            
         end
 
-        %Apply the input
+        %Apply the input and store applied input at time t
         x = RK4(x,uu(:,1),h,mpciter);
         u(mpciter) = uu(:,1);
-        % Declare new inital conditions
-        mpciter = mpciter + 1;
-        x0 = x(mpciter,:)';
         
-        %X0 = [xx(2:end,:);xx(end,:)];
+        %declare new inital conditions and search space, process to next iteration
+        x0 = x(mpciter+1,:)';
         X0 = xx;
         
-
         
     end
-    mpciter = mpciter - 1;
+    
+
     xopt = zeros(mpciter*nx,N+1);
     uopt = zeros(mpciter*nu,N);
+    
+    %Reshaping the storage matrix s.t it is on the form x1:;x2:;...
     for i = 1:nx
        xopt((i-1)*mpciter +1:i*mpciter ,:) = reshape(xopttmp(i:nx:mpciter*nx,:),size(xopttmp(i:nx:mpciter*nx,:),1),N+1);
     end
     for i = 1:nu
        uopt((i-1)*mpciter +1:i*mpciter ,:) = reshape(uopttmp(i:nu:mpciter*nu,:),size(uopttmp(i:nu:mpciter*nu,:),1),N);
     end    
+    %Extracing only valid points, if the simulation was finished before
+    %tspan
     t = (0:h:(mpciter)*h + T)';
-    x = x(1:mpciter+1,:);
-    u = u(1:mpciter+1,:);
+    x = x(1:mpciter,:);
+    u = u(1:mpciter,:);
     
 end
 
 function x = RK4(x,u,dt,t)
+%Numerical Simulation method, Runge kutte of 4th order
     k1 = dt*PlantMDS(x(t,:)',u);
     k2 = dt*PlantMDS(x(t,:)' + k1/2,u);
     k3 = dt*PlantMDS(x(t,:)' + k2/2,u);
@@ -143,36 +159,46 @@ end
     
 function [L,x,u,chi,cost] = Lagrangian(T,N,nx,nu,Q,R,A,B,xx,uu,sol,args,nlpProb)
 %Number of eqconstraints = N*nx  <=> Number of lagrange multipliers for eq
+%Solver uses X0 as a decision variable, and an equality constraint? to 
+%Ensure that it = x0. nx new multipliers and nx constraints are therefore
+%added
 %% Initialization of variables
-    syms x   [N+1 nx] real; x=x';        %State variables
-    syms u   [N nu]   real; u=u';       %Input variables
-    syms chi [N nx] real; %Lagrange multipliers for function eq constraints
-    z = [x(:,2:end);u];         %Decision variables vector
-    cost = sym('0'); 
-    constraints(1:N*nx,1) = sym('0');
-    vars = [x;u,0;chi']
-    nums 
-    chi = reshape(chi',N*nx,1);
+    x0 = xx(:,1);
+    syms x   [N+1 nx] real; x=x';           %State variables
+    syms u   [N+1 nu]   real; u=u';         %Input variables
+    syms chi [N+1 nx] real;                 %Lagrange multipliers for function eq constraints
+    z = [x(:,2:end);u(1:end-1)];            %Decision variables vector(That are in the cost)
+    vars = [x;u;chi'];                      %Matrix of differentiable variables
+    nums = full([xx;[uu,0];reshape(sol.lam_g,2,N+1)]);% Matrix of numerical values for diff variables
+    chi = reshape(chi',(N+1)*nx,1);         %Reshaping the multipliers to correct form
+    cost = sym('0');                        %Cost / stage function
+    constraints(1:N*nx,1) = sym('0');       %Vector of all equality constraints
 %% Calculation of symbolic lagrangian
     for k = 1:N % Sum of cost, from 0 to N-1
-        cost = cost + z(1:nx,k)'*Q*z(1:nx,k) + z(nx+1:nx+nu,k)'*R*z(nx+1:nx+nu,k);
-        constraints((k-1)*nx+1:k*nx) = A*x(:,k) + B*u(:,k) - x(:,k+1);
+        cost = cost + 0.5*(z(1:nx,k)'*Q*z(1:nx,k) + z(nx+1:nx+nu,k)'*R*z(nx+1:nx+nu,k));
+        constraints((k-1)*nx+1:k*nx) = x(:,k) +  T/N*(A*x(:,k) + B*u(:,k)) - x(:,k+1);
     end
-    L = cost + chi'*constraints;
+    constraints = [x0 - x(:,1);constraints];
+    L = cost - chi'*constraints;
+
 
 %% Calculation of symbolic gradients
-    J(nx+nu,N,1) = sym('0');
-    Jrow(1,N) = sym('0');
+    J(nx+nu,N+1,1) = sym('0');
+        Jrow(1,N+1) = sym('0');
     for row = 1:nx+nu
-        
-        for col = 1:N
-            Jrow(1,col) = diff(L,z(row,col),1);
+
+        for col = 1:N+1
+            Jrow(1,col) = diff(L,vars(row,col),1);
         end
         J(row,:) = Jrow;
     end
-    J
-
-
+    fprintf("Solution cost                 : %6.2f \n",full(sol.f))
+    fprintf("Solution cost - evaluated cost: %6.2f \n",full(sol.f)-eval(subs(cost,vars,nums)))
+    fprintf("Symbolic Lagrangian           : %s \n",L)
+    fprintf("Symbolic Gradient at optimum  : \n \n")
+        disp(J)
+    fprintf("Numerical Gradient at optimum : \n \t")
+        disp(eval(subs(J,vars,nums)))
 
 end
    
--- MPCTests/MassSpringDamper/main.m
+++ MPCTests/MassSpringDamper/main.m
@@ -9,18 +9,21 @@
 %%
 nx = 2; nu = 1;
 N = 10; T = 1; 
-A = [0 1; -1 -1];
+A = [0 1; 1 1];
 B = [0;1];    
-Q = diag([20 2]);
+Q = diag([20 40]);
 R = 0;
 x0 = [1;0.5];
-tspan = 3;
+tspan = 10;
 h = 0.01;
 xRef= [0;0];
 Xlb = []; Xub = [];
 Ulb = []; Uub = [];
 [x,u,xopt,uopt,t] = Simulation(A,B,Q,R,x0,xRef,Xlb,Xub,Ulb,Uub,h,tspan,N,T,nx,nu);
 %%
+figure(2)
+clf(2)
+plot(t,xopt(size(xopt,1)/2+1:10:end,'b-.');
 figure(1)
 if(0)
     for i = 1:size(x,1)
@@ -54,7 +57,7 @@ for i = 1:size(x,1)
    plot(t(i),x(i,1),'b.'); hold on; grid on
    if ~isnan(xopt(i,:)) 
         
-       delete(opt);
+        delete(opt);
         delete(inp);       
         opt = plot(t(i:T/N/h:i+T/h),xopt(i,:),'b-.');
         inp = plot(t(i:T/N/h:i+(T-T/N)/h),uopt(i,:),'r-.');
